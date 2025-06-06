7f9f94f6287ea4ef0d648dc2055b2984dc5db678
AtulSingh-alt <atul061212@gmail.com>
2025-06-06 15:24:15 +0530

    First program  Commit

---
+++ 1_Calculate_grades.py
@@ -0,0 +1,15 @@
+# 1) Calculate the grade with if else
+from selectors import SelectSelector
+
+marks = int(input("Enter your score : "))
+if marks >= 90:
+    print("Your grade is A")
+elif marks >= 80:
+    print("Your grade is B")
+elif marks >= 70:
+    print("Your grade is C")
+elif marks >= 55:
+    print("Your grade is D")
+else:
+    print("Sorry! You are fail")
+SelectSelector
