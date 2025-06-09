8965575543db848aad37c4ec4e7e5fedb934dd10
AtulSingh-alt <atul061212@gmail.com>
2025-06-09 11:51:18 +0530

    Added new program

---
+++ 3_Largest_of_two_number.py
@@ -0,0 +1,8 @@
+# 3) the Largest of Two Numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    print("The larger number is:", a)
+else:
+    print("The larger number is:", b)
